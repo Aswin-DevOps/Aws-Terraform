aec6fe2e9a69b0cd7ff56252b2e163b90da138ce
aswin9934048 <aswin@digivalsolutions.com>
2025-12-12 17:57:46 +0530

    modified mmodule VPC

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -2,9 +2,9 @@ output "public_subnets" {
   value = aws_subnet.public_subnets
 }
 
-#output "private_subnets" {
-#  value = aws_subnet.private_subnets
-#}
+output "private_subnets" {
+  value = aws_subnet.private_subnets
+}
 
 output "vpc_id" {
   value = aws_vpc.vpc.id
